9410fa83a4549697d0300f808b76524874980727
Jason Belgard <belgard.jason@gmail.com>
2021-09-20 20:20:24 -0500

    assignment done

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log (15);
+
+// Primitive Data Types: String, Number, Boolean, Undefined, Null
+
+//  Operator symbols: -, +, *, /, **, %
+
+console.log(5 % 4);
+console.log(5 % 2);
+
+console.log('I\'M MARRY POPPINS, Y\'ALL\!')
+
+console.log('Hawk' + ' eye');
+
+console.log('Jason'.length + 'Belgard'.length);
+
+console.log('Avengers: Endgame'[16]);
+
+console.log('\\');
+
+console.log((5+9)**4);
+
+console.log([('I'.length + 'Am'.length) + 'Iron'.length] % 'Man'.length);
+
+console.log('CAP'[1])
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('Nick Fury'[0]);
+console.log('Groot'[0]);
+console.log('Doctor Strange'[13]);
+console.log('Thor'[3]);
+console.log('Star-Lord'[0]);
+
+m = 25;
+c = 4;
+console.log(m + c);
